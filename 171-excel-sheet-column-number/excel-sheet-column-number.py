1a11e080d503d5306654d6802ae80ffa79b2c536
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-04-20 15:14:32 -0500

    Time: 36 ms (59.77%) | Memory: 16.6 MB (36.88%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/excel-sheet-column-number.py
@@ -1,13 +1,10 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         #O(n) time complexity 
-        definitions = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26 }
-        s = columnTitle[::-1]
-        res = 0
-        for i in range(len(s)):
-           print(s[i])
-           print(definitions[s[i]] * (26 ** i))
-
-           res += definitions[s[i]] * (26 ** i)
-
-        return res
+        ans, pos = 0, 0
+        for letter in reversed(columnTitle):
+            digit = ord(letter)-64
+            ans += digit * 26**pos
+            pos += 1
+            
+        return ans
