8b8ccc6b46f611bdaf2fce2a98739bcd292dc62c
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-04-21 14:01:03 -0500

    Time: 277 ms (79.26%) | Memory: 32 MB (71.01%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -3,29 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-#Non-trivial approach
 class Solution:
-    def reverse(self, head: ListNode) -> ListNode:
-        prev = None
-        curr = head
-        while curr:
-            next_temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_temp
-        return prev
-
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
+
+        #Find reverse 
+        def reverse(head): 
+            prev, curr = None, head
+            while curr:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+            return prev
+        
+
+        #Find mid
+        slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        rev = self.reverse(slow)
+        
+        rev = reverse(slow)
+
         while rev:
             if head.val != rev.val:
                 return False
             head = head.next
             rev = rev.next
         return True
+
