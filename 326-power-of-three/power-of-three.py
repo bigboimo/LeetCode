45becce1c82739b20d219076ecb849f43f3a3d7d
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-04-24 11:03:04 -0500

    Time: 50 ms (95.56%) | Memory: 16.5 MB (77.69%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,19 +1,6 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        num = (-math.inf)
-        powr = 0
-
-        while num < n:
-            num = 3 ** powr
-            
-            if num == n:
-                return True
-            
-            powr += 1
-        
-        return False
-
-        ## Mathematical approach since upper bound is 2^31 - 1 and 3^20 is larger, if n
-        ## is a power of three then it will divide without decimals which is why modulo
-        ## works perfectly. 
-        ## return n > 0 and 3**20 % n == 0
+        if n < 1:
+            return False
+        else:
+            return (3**31) % n == 0
