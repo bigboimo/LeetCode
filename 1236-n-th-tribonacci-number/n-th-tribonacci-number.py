8a2af125dd5c3e57b95f801e41e1bee2d4ab082b
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-04-24 10:41:39 -0500

    Time: 28 ms (91.21%) | Memory: 16.5 MB (31.32%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,17 +1,18 @@
 class Solution:
-    #Optimal time and space solution
-       def tribonacci(self, n: int) -> int:
+    def tribonacci(self, n: int) -> int:
         if n == 0:
             return 0
-        elif n == 1 or n == 2:
+
+        if n == 1 or n == 2:
             return 1
-        
+
         a, b, c = 0, 1, 1
         
+        #Storing only last 3 sub-problems for most optimal space complexity. T(n) =  T(n-1) + T(n-2) + T(n-3). This recurrence shows we only need last 3 subproblem solutions therefore a hashset is not needed. 
         for i in range(3, n + 1):
             next_trib = a + b + c
             a = b
             b = c
             c = next_trib
-        
+
         return c
