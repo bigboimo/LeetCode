00bdfa6243d0d8f263f7b4da8e8f82c175eab3ba
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-04-30 18:02:08 -0500

    Time: 93 ms (44.15%) | Memory: 22.3 MB (21.50%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
+        #O(m * n) optimized solution
         hashmap = defaultdict(list)
+        
 
-        for i in range(len(strs)):
-            curr = ''.join(sorted(strs[i]))
-            hashmap[curr].append(strs[i])
+        for l in strs:
+            res = [0] * 26 
+            
+            for s in l:
+                count = ord(s) - ord('a') 
+                res[count] += 1
+            
+            hashmap[tuple(res)].append(l)
 
         return hashmap.values()
 
-            
+
