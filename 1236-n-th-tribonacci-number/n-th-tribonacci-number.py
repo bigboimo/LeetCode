b05c4fbb3c485edc79e4af23122fbc84ff03564a
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-05-09 15:11:47 -0500

    Time: 33 ms (67.13%) | Memory: 16.4 MB (84.26%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -3,9 +3,6 @@ class Solution:
         if n == 0:
             return 0
 
-        if n == 1 or n == 2:
-            return 1
-
         a, b, c = 0, 1, 1
         
         #Storing only last 3 sub-problems for most optimal space complexity. T(n) =  T(n-1) + T(n-2) + T(n-3). This recurrence shows we only need last 3 subproblem solutions therefore a hashset is not needed. 
