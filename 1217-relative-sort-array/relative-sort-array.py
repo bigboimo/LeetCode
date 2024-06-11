44bf119c58c6ea9db46a90f219fdc41801883257
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-06-11 10:22:01 -0400

    Time: 34 ms (88.88%) | Memory: 16.8 MB (10.78%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,9 +1,5 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        def addNum(n, times,list):
-            for i in range(times):
-                list.append(n)
-        
         arr1Counts = Counter(arr1)
         added = set()
         sortArr = []
@@ -11,14 +7,16 @@ class Solution:
 
 
         for n in arr2:
+
             if n in arr1Counts and n not in added:
-                addNum(n,arr1Counts[n], res)
-                added.add(n)
+                res.extend([n] * arr1Counts[n])
+            elif n not in added:
+                sortArr.add(n)
+            added.add(n)
             
         for n in arr1:
             if n in added:
                 continue
-            
             sortArr.append(n)
 
         print(res)
