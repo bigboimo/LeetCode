718d296687eaa8a4b9f329748729554bf19f5329
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2024-06-20 16:03:55 -0400

    Time: 36 ms (95.15%) | Memory: 19 MB (57.16%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -7,20 +7,12 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
-        intersectionFound = False
 
-        #Loop to determine intersection of slow and fast pointers, if no cycle the loop ends as a null node is eventually visited. O(n) time complexity
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
             if slow == fast:
-                intersectionFound = True
-                break
+                return True
 
-        return intersectionFound
-
-
-            
-
-        
+        return False
