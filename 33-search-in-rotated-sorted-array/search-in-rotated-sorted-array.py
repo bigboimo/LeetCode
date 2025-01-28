4ea23064849a32d77008cd297a54f0e10e369050
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2025-01-28 13:19:17 -0600

    Time: 0 ms (100.00%) | Memory: 18.3 MB (16.18%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,49 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
         l, r = 0, len(nums) - 1
+
+        minVal = nums[l]
+        index = 0
+
+        res = -1
+
+        #Find minimum value in logn
+        while l <= r:
+            mid = (l+r) // 2
+            
+            if nums[mid] < minVal:
+                minVal = nums[mid]
+                index = mid
+                
+            #Check if in sorted region or not
+            if nums[mid] >= nums[l] and nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        print(minVal)
+
+        #Typical binary search
+
+        if nums[index] <= target <= nums[-1]:
+            l, r = index, len(nums) - 1
+        else:
+            l, r = 0, index - 1
+
 
         while l <= r:
             mid = (l+r)//2
+            print(nums[l], nums[mid], nums[r])
 
-            print(l, mid, r)
-
-            if nums[mid] == target:
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else: 
                 return mid
-
-            #left-sorted side
-            if nums[mid] >= nums[l]:
-                print('left')
-                if nums[l] <= target <= nums[mid]:
-                    r = mid - 1
-                else: 
-                    l = mid + 1
-            #right-sorted side
-            else:
-                print('right')
-                if nums[mid] <= target <= nums[r]:
-                    l = mid + 1
-                else: 
-                    r = mid - 1
+        
+        return res
             
 
-        return -1
 
