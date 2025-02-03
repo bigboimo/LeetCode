20a24bc2c1b239b5d0a23ff47ddff7a208d52524
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2025-02-03 15:18:12 -0600

    Time: 0 ms (100.00%) | Memory: 23.3 MB (67.79%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -3,37 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next:
-            return
 
-        # Step 1: Find the middle of the linked list
-        slow, fast = head, head
+        slow, fast = head, head.next
+        #Find middle
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # Step 2: Reverse the second half of the list
-        prev, curr = None, slow.next
-        slow.next = None  # Disconnect the first and second halves
+        second = slow.next
+        #Disconnect first from second
+        slow.next = None
 
+        #Reverse second part of the linked list
+        prev, curr = None, second
         while curr:
             temp = curr.next
             curr.next = prev
             prev = curr
             curr = temp
-
-        # Step 3: Merge the two halves
-        first, second = head, prev
+        
+        second = prev
+        first = head
+        
+        #Merging the two lists
         while second:
-            temp1, temp2 = first.next, second.next
+            temp1 = first.next
             first.next = second
-            second.next = temp1
-
             first = temp1
+
+            temp2 = second.next
+            second.next = first
             second = temp2
+
+        
