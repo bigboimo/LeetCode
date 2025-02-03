184ecde5f178e5713a65860699c927e18df04cd4
Mohamed Elaagip <118075696+bigboimo@users.noreply.github.com>
2025-02-03 09:59:28 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (60.86%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,28 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr = dummy = ListNode()
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        curr1 = list1
+        curr2 = list2
+        merged = ListNode(0)
+        res = merged
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                curr = list1
-                list1 = list1.next
+        while curr1 and curr2:
+            if curr1.val <= curr2.val:
+                res.next = curr1
+                curr1 = curr1.next
             else: 
-                curr.next = list2
-                curr = list2
-                list2 = list2.next
-            
-        if list1:
-            curr.next = list1
+                res.next = curr2
+                curr2 = curr2.next
+            res = res.next
 
-        if list2: 
-            curr.next = list2
+        if curr1:
+            res.next = curr1
         
-        print(dummy)
+        if curr2:
+            res.next = curr2
 
-        return dummy.next
+        return merged.next
+
+        
